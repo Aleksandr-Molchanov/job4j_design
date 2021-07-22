2c3eeb8d884d023959da9ede54e2759f3fb83a6e
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-07-22 16:24:33 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+            PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)
+                ))) {
+            for (String str = read.readLine(); str != null; str = read.readLine()) {
+                boolean trigger = true;
+                if (str.startsWith("400") || str.startsWith("500")) {
+                    String[] logStart = str.split(" ");
+                    out.write(logStart[1]);
+                    out.print(";");
+                    trigger = false;
+                }
+                if (!trigger && str.startsWith("200") || str.startsWith("300")) {
+                    String[] logStop = str.split(" ");
+                    out.write(logStop[1]);
+                    out.println(";");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
