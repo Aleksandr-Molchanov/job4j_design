35f29c4bf94b2d7d0c30acbfcfa0ddb246cefc9a
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-07-23 09:55:48 +0300

    3.0. Тестирование IO [#173905]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -3,7 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analizy {
-    public void unavailable(String source, String target) {
+    public static void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
             PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
@@ -11,13 +11,13 @@ public class Analizy {
                 ))) {
             boolean trigger = true;
             for (String str = read.readLine(); str != null; str = read.readLine()) {
-                if (trigger && str.startsWith("400") || str.startsWith("500")) {
+                if (trigger && (str.startsWith("400") || str.startsWith("500"))) {
                     String[] logStart = str.split(" ");
                     out.write(logStart[1]);
                     out.print(";");
                     trigger = false;
                 }
-                if (!trigger && str.startsWith("200") || str.startsWith("300")) {
+                if (!trigger && (str.startsWith("200") || str.startsWith("300"))) {
                     String[] logStop = str.split(" ");
                     out.write(logStop[1]);
                     out.println(";");
