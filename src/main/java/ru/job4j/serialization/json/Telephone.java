2ad87dd78d0f6a62bc8bb645f32619623fc75acf
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-08-06 13:39:08 +0300

    3. Формат XML [#313165]

--- src/main/java/ru/job4j/serialization/json/Telephone.java
+++ src/main/java/ru/job4j/serialization/json/Telephone.java
@@ -1,8 +1,7 @@
-package ru.job4j.serialization;
+package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import ru.job4j.serialization.json.Manufacturer;
 
 import java.util.Arrays;
 
