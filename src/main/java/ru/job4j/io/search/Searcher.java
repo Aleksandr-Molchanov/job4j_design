e1818b9cd44504565b367f00e50644e0ec48ec8c
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-08-24 17:29:43 +0300

    2. Поиск файлов по критерию [#783]

---
+++ src/main/java/ru/job4j/io/search/Searcher.java
@@ -0,0 +1,46 @@
+package ru.job4j.io.search;
+
+import ru.job4j.io.ArgsName;
+import ru.job4j.io.Search;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Searcher {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Ошибка параметров запуска");
+        }
+        ArgsName param = ArgsName.of(args);
+        List<Path> paths;
+        Path start = Paths.get(param.get("d"));
+        switch (param.get("t")) {
+            case "mask" -> paths = Search.search(start, p -> p.toFile().getName().endsWith(param.get("n")));
+            case "name" -> paths = Search.search(start, p -> p.toFile().getName().equals(param.get("n")));
+            case "regex" -> {
+                Pattern pattern = Pattern.compile(param.get("n"));
+                paths = Search.search(start, p -> pattern.matcher(p.toFile().getName()).find());
+            }
+            default -> throw new IllegalArgumentException("Ошибка параметра поиска");
+        }
+        writeFile(paths, new File(param.get("o")));
+    }
+
+    public static void writeFile(List<Path> file, File out) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(out))) {
+            for (Path path : file) {
+                pw.write(path.toFile().getName());
+                pw.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
