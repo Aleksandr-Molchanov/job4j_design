2a61b15f72762500362c43b6b5ca3ac67dc9e770
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-10-11 13:39:49 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #214073]

--- src/main/java/ru/job4j/design/srp/Report.java
+++ src/main/java/ru/job4j/design/srp/Report.java
@@ -1,9 +1,10 @@
 package ru.job4j.design.srp;
 
+import javax.xml.bind.JAXBException;
 import java.util.function.Predicate;
 
 public interface Report {
 
-    String generate(Predicate<Employee> filter);
+    String generate(Predicate<Employee> filter) throws JAXBException;
 
 }
--- src/main/java/ru/job4j/design/srp/XMLReportEngine.java
+++ src/main/java/ru/job4j/design/srp/XMLReportEngine.java
@@ -16,20 +16,18 @@ public class XMLReportEngine implements Report {
     }
 
     @Override
-    public String generate(Predicate<Employee> filter) {
-        StringBuilder xml = new StringBuilder();
-        for (Employee employee : store.findBy(filter)) {
-            JAXBContext context = null;
-            try (StringWriter writer = new StringWriter()) {
-                context = JAXBContext.newInstance(Employee.class);
-                Marshaller marshaller = context.createMarshaller();
-                marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-                marshaller.marshal(employee, writer);
-                xml.append(writer.getBuffer().toString());
-            } catch (JAXBException | IOException e) {
-                e.printStackTrace();
-            }
+    public String generate(Predicate<Employee> filter) throws JAXBException {
+        Employees employees = new Employees(store.findBy(filter));
+        JAXBContext context = JAXBContext.newInstance(Employees.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(employees, writer);
+            xml = writer.getBuffer().toString();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return xml.toString();
+        return xml;
     }
 }
