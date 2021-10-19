90860d271ad281958c436d9a3c2bec80b7768e93
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-10-19 15:32:59 +0300

    1. Хранилище продуктов [#852 #217137]

--- src/main/java/ru/job4j/ood/lsp/storage/ControllQuality.java
+++ src/main/java/ru/job4j/ood/lsp/storage/ControllQuality.java
@@ -1,14 +1,23 @@
 package ru.job4j.ood.lsp.storage;
 
+import java.util.List;
+
 public class ControllQuality {
 
-    private Strategy strategy;
+    private List<Strategy> strategy;
 
-    public ControllQuality(Strategy strategy) {
+    public ControllQuality(List<Strategy> strategy) {
         this.strategy = strategy;
     }
 
-    public boolean executeStrategy(Food food) {
-        return strategy.add(food);
+    public boolean distribute(Food food) {
+        boolean rsl = false;
+        for (Strategy st : strategy) {
+            if (st.add(food)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
     }
 }
