ea71891e92642da4bcf197a6fa37364f6c69c9bb
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-10-21 17:56:00 +0300

    1. Хранилище продуктов [#852 #217137] Правки.

--- src/main/java/ru/job4j/ood/lsp/storage/ControllQuality.java
+++ src/main/java/ru/job4j/ood/lsp/storage/ControllQuality.java
@@ -1,23 +1,35 @@
 package ru.job4j.ood.lsp.storage;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ControllQuality {
 
-    private List<Strategy> strategy;
+    private List<Storage> storages = new ArrayList<>();
 
-    public ControllQuality(List<Strategy> strategy) {
-        this.strategy = strategy;
+    public ControllQuality(List<Storage> storage) {
+        this.storages = storage;
     }
 
     public boolean distribute(Food food) {
         boolean rsl = false;
-        for (Strategy st : strategy) {
+        for (Storage st : storages) {
             if (st.add(food)) {
                 rsl = true;
                 break;
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        Bread bread = new Bread("bread", new Date(), new Date(), 10.99, false);
+        Eggs eggs = new Eggs("eggs", new Date(), new Date(), 10.99, false);
+        Meat meat = new Meat("meat", new Date(), new Date(), 10.99, false);
+        ControllQuality control = new ControllQuality(List.of(new Warehouse(), new Shop(), new Trash()));
+        control.distribute(bread);
+        control.distribute(eggs);
+        control.distribute(meat);
     }
 }
